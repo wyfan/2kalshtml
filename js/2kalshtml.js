b69843f9b7e7565c829870ec6c3c92388a568d57
wyfan <moonunique@gmail.com>
2019-11-13 15:06:27 +0800

    20191113 改用SUMMERNOT LITE，不使用Bootstrap，因為要使用SEMANTIC UI

--- js/2kalshtml.js
+++ js/2kalshtml.js
@@ -19,6 +19,7 @@ $(function(){
 
   $('#btnCreatFile').click(function(){
     console.log("有沒有抓到TEXT啊：" + $("#showContent").text());
+    console.log("summernot text:" + $('#summernote').summernote('code'));
     var kalsUrl = "http%3A%2F%2Fexp-kals-2012.dlll.nccu.edu.tw%2Fkals%2Fweb_apps%2Fgeneric%2Floader%2Frelease";
     //---開始組成HTML(Header部份)-------
     var htmlHeader = "<html><head><meta charset='utf-8'><title>kals文本</title></head><body>";
